0ac291c58672be4f2be8d5ee721381cd3cfc23e8
Emerzz35 <160292711+Emerzz35@users.noreply.github.com>
2025-03-15 13:25:03 -0300

    HTML user-create
    - Cria o HTML do user-create
    - Cria a rota user-insert

--- resources/views/user-create.blade.php
+++ resources/views/user-create.blade.php
@@ -2,5 +2,14 @@
 
 @section('content')
 {{-- Fazer a pagina aqui --}}
-criar
+<main>
+    <form action="{{ route('user-insert') }}" method="POST">
+        @csrf    
+        <input type="text" name="name" placeholder="Seu nome">   
+        <input type="text" name="email" placeholder="Seu Email">  
+        <input type="password" name="password" placeholder="Sua senha">
+        <span>Já tem uma conta? ><a href="#">Entrar</a></span>
+        <x-button class='' linkto='user-insert'>Criar nova conta</x-button>
+    </form>
+</main>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () { return view('sobre');});
 
 Route::get('/criar-conta', function () { return view('user-create'); })->name('user-create');
+Route::post('/criar-conta', function () { return 'validação e inserção do usuario'; })->name('user-insert');
