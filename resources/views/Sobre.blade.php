e1754b14b7f2282de63896d5406ad43d628cdb3b
Emerzz35 <160292711+Emerzz35@users.noreply.github.com>
2025-03-15 11:29:55 -0300

    Instalação do Laravel
    Instala o Laravel configurado para SQLite

---
+++ resources/views/Sobre.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="Pt-Br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pride Path</title>
+</head>
+<body>
+ <h1>Sobre Nós aa</h1>   
+</body>
+</html>
