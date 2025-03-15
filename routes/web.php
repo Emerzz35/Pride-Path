12e061246f5ba04b0543295f97f5704b0e5ec8c6
Emerzz35 <160292711+Emerzz35@users.noreply.github.com>
2025-03-15 13:00:42 -0300

    Ajuste nas rotas

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('sobre');
-});
+Route::get('/', function () { return view('sobre');});
 
-Route::get('/criar-conta', function () {
-    return view('user-create');
-});
+Route::get('/criar-conta', function () { return view('user-create'); })->name('user-create');
